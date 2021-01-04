6bfb0acf607dda63df05999bdb748d971e2582a6
Stiven Rodriguez <judrobinx@gmail.com>
2021-01-04 11:11:22 -0500

    feat (): implement component in examples

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
-import { ExampleComponent } from '@berserk-code/react-clock'
+import Clock from '@berserk-code/react-clock'
 import '@berserk-code/react-clock/dist/index.css'
 
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const color = '#59df7a';
+  const dateColor = '#09b8be';
+  return <Clock
+    color={color}
+    dateColor={dateColor}
+    locales={['es-ES']}
+    optionsLocaleDate={{ weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }}
+    optionLocaleTime={{ hour12: true, hour: '2-digit', minute: '2-digit', second: '2-digit' }}
+  />
 }
 
 export default App
